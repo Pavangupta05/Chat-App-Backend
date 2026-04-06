bd63549170cff62f937fac84d4b641605f212cfc
Pavangupta05 <pavangupta150605@gmail.com>
2026-04-06 23:32:12 +0530

    fix(cors): properly add vercel https domain to allowlist

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ const allowedOrigins = [
   "http://127.0.0.1:3000",
   "http://127.0.0.1:5173",
   "https://chat-app-frontend-eqb345dra-pavan-kumar-gupta-s-projects.vercel.app",
+  "https://chat-app-frontend-kappa-tan.vercel.app",
 ];
 
 app.use(
